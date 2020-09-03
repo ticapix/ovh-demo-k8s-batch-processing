29d37fd3a7e6e71380b3e82fe1bd3b4dd91fac99
Pierre Gronlier <pierre.gronlier@ovhcloud.com>
2020-09-03 11:49:02 +0000

    docker image of the video task

---
+++ Makefile
@@ -0,0 +1,45 @@
+ROOT_DIR:=$(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
+NAME=$(shell basename $(ROOT_DIR))
+REPO?=ticapix/$(NAME)
+TAG?=latest
+RM=rm -rf
+
+.PHONY: help
+
+help:
+	@echo "$(NAME)"
+	@grep -E '^[a-zA-Z0-9_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-15s\033[0m=> %s\n", $$1, $$2}'
+
+venv3: # create venv folder
+	python3 -m venv venv3
+	./venv3/bin/pip install -Ur requirements.txt
+
+deps: venv3
+
+docker-build: deps ## build docker image
+	docker build -f task/Dockerfile -t $(REPO):master task
+	docker tag $(REPO):master $(REPO):$(TAG)
+
+docker-test: clean docker-build 
+	env | grep OS_ > vars.env
+	docker run --env-file ./vars.env $(REPO) batch_processing 'videos/PRIVATE 1/AVCHD/BDMV/STREAM/00108.MTS' batch_processing_result
+
+# docker-enter-image: docker-build  ## for local manual testing
+# 	docker run -it --entrypoint sh $(REPO):master
+
+docker-push: docker-build ## push docker image to hub.docker.io
+	docker push $(REPO)
+
+# deploy: ## deploy plugin on the cluster
+# 	kubectl apply -f setup/service-account.yaml
+# 	kubectl apply -f setup/storageos-monitoring.yaml
+
+# undeploy: ## remove plugin from the cluster
+# 	kubectl delete -f setup/storageos-monitoring.yaml || true
+# 	kubectl delete -f setup/service-account.yaml || true
+
+# watch-log:
+# 	kubectl -n storageos-operator logs -f `kubectl -n storageos-operator get pod -l app=storageos-monitoring -o jsonpath='{.items[0].metadata.name}'`
+	
+clean: ## remove development files
+	$(RM) ./venv3
---
+++ task/transcode.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+set -ex
+
+if [ $# -ne 3 ]; then
+    echo "wrong argument. usage $0 <input_bucket> <file_path> <output_bucket>"
+    exit 1
+fi
+
+input_bucket=$1
+input_filepath=$2
+output_bucket=$3
+
+# Does the input file exist ?
+if ! swift stat "$input_bucket" "$input_filepath"; then
+    echo "Input file $input_bucket $input_filepath doesn't exist"
+    exit 1
+fi
+
+output_filepath="${input_filepath%.*}.webm"
+
+# Does the output file exist ?
+if swift stat "$output_bucket" "$output_filepath"; then
+    echo "Output file $output_bucket $output_filepath already exists"
+    exit 0
+fi
+
+# Is the input file available localy ?
+if ! ls "input.mp4"; then
+    # transcode file on the fly to
+    # - avoid IO access
+    # - reduce disk space (reduce FPS and resolution)
+    # - speed up seeks later
+    swift download "$input_bucket" "$input_filepath" --output - | ffmpeg -i - -c:v libx265 -an -sn -x265-params crf=25 -preset ultrafast -r 30 -vf scale=320:-2 -y input.mp4
+    input_filepath=input.mp4
+fi
+
+#docker run -v $(pwd):$(pwd) -w $(pwd) jrottenberg/ffmpeg:4.1-scratch -i "$input_filepath" -c:v libx265 -c:a copy -x265-params crf=25 -y output.mp4
+
+# get file metadata
+ffprobe -v quiet -hide_banner -loglevel fatal -show_streams -print_format json -i "$input_filepath" > data.json
+
+parts=12
+# take duration of the first video stream
+duration=`cat data.json | jq -r '[.streams[] | select(.codec_type == "video")][0] | .duration'`
+
+# generate $parts of 1 seconde
+rm files.txt || true
+for i in $(seq 1 $parts); do
+    echo "file 'output-$i.mp4'" >> files.txt
+    start_time=`echo "($duration)/$parts*($i-1)" | bc -l`
+    ffmpeg -i "$input_filepath" -an -sn -ss $start_time -t 1 -r 30 -vf scale=320:-2 -y output-$i.mp4
+done
+
+# concatenate the parts
+ffmpeg -f concat -i files.txt -c:v libvpx-vp9 -crf 30 -b:v 0 -y output.webm
+
+# upload result
+swift upload "$output_bucket" --object-name "$output_filepath" output.webm
